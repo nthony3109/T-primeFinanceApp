c910d3d93ad366c939775e8fd0ad45c4b7026d70
nthony3109 <chibuzoranthonyajibo@gmail.com>
2026-08-17 22:33:34 +0100

    dashboard design

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,23 @@
-import Card from "./Card"
+import Card from "../components/Card"
+import RecentTnx from "../components/RecentTnx"
+import { NavLink } from "react-router-dom"
 
-const Dashboard = () => {
+const Dashboard = ({transactions}) => {
+
   return (
-    <div>   
+    <div className="bg-slate-200 h-max  w-screen p-2 md:pt-0  ">   
         <div className="font-sans m-5">
           <h1 className="text-lg font-bold">Welcome,track your finances</h1>
           <p className="text-slate-500 ml-2 text-sm font-semibold">Here is your financial overview</p>
         </div>
-        <Card />
+        <Card tnx={transactions} /> 
+        <div>
+           <div className="flex justify-between items-center p-2 -mt-8">
+            <h1 className="font-semibold font-sans">Recent Transactions</h1>
+            <NavLink to="/transactions" className=" text-blue-500 p-2 font-semibold cursor-pointer  font-sans rounded-md">View All</NavLink>
+           </div>
+          <RecentTnx transactions={transactions} />
+        </div>
     </div>
   )
 }
